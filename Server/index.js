767e1ec4ffc755a3b356bb2a9c581a3a73007cdd
priyanshu-gupta07 <guptapiyush.0707@gmail.com>
2024-08-16 01:09:34 +0530

    Update vercel config

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000', // your frontend URL
+    origin: 'https://crypto-bazar-55e3.vercel.app',
     credentials: true
   }));
 app.use(express.urlencoded({extended: true}));
